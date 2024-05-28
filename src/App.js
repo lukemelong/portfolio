6cf201a4f6f3ba28d5f3a7c9cf3137f85a61862b
Luke Melong <43002203+lukemelong@users.noreply.github.com>
2024-05-28 12:39:58 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="App">
+      This is a test
+    </Container>
+  );
+}
+
+export default App;
